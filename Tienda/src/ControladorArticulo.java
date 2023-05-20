1a9163366c1cf707c8298dcfcc19fb6e63ca0f9c
EmmanuelCetzal <bryanemmanuel1209@gmail.com>
2023-05-20 10:23:32 -0600

    MVC de la clase artículo

--- Tienda/src/ControladorArticulo.java
+++ Tienda/src/ControladorArticulo.java
@@ -1,19 +1,21 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ControladorArticulo {
-    private List<Articulo> carrito;
+    private List<Articulo> inventario;
     private VistaArticulo vista;
+    private Articulo aux;
 
     public ControladorArticulo() {
-        carrito = new ArrayList<>();
+        inventario = new ArrayList<>();
         vista = new VistaArticulo();
     }
 
     public void agregarArticulo(){
-        Articulo articulo = new vista.obtenerDatosArticulo();
-        carrito.add(articulo);
-        System.out.println("Artículo agregado al carrito: " + articulo.getNombreArticulo());
+        aux = vista.obtenerDatosArticulo();
+        inventario.add(aux);
+        System.out.println("Artículo agregado al carrito: " + aux.getNombreArticulo());
     }
 
     public void iniciar() {
@@ -25,27 +27,56 @@ public class ControladorArticulo {
 
             switch (opcion) {
                 case 1:
-                    vista.mostrarArticulos(carrito);
+                    vista.mostrarArticulos(inventario);
                     break;
                 case 2:
                     agregarArticulo();
                     break;
                 case 3:
-                    actualizarArticulo();
+                    vista.modificarArticulo();
                     break;
                 case 4:
+                    eliminarArticulo();
+                    break;
+                case 5:
                     vista.mostrarMensaje("¡Saliendo del menu de artículo!");
                     break;
                 default:
                     vista.mostrarMensaje("Opción inválida");
                     break;
             }
-        } while (opcion != 4);
+        } while (opcion != 0);
     }
 
-    private void actualizarArticulo() {
+    public void modificarArticulo(int identificador, String nuevoNombre, double nuevoPrecioPublico, double nuevoPrecioProveedor, int nuevaCantidadExistencia) {
+        Articulo articulo = buscarArticuloPorId(identificador);
+        if (articulo != null) {
+            articulo.setNombreArticulo(nuevoNombre);
+            articulo.setPrecioPublico(nuevoPrecioPublico);
+            articulo.setPrecioProveedor(nuevoPrecioProveedor);
+            articulo.setStock(nuevaCantidadExistencia);
+            System.out.println("El artículo se ha modificado correctamente.");
+        } else {
+            System.out.println("No se encontró ningún artículo con el identificador especificado.");
+        }
+    }
+
+    public Articulo buscarArticuloPorId(int identificador) {
+        for (Articulo inventario : inventario) {
+            if (inventario.getId() == identificador) {
+                return inventario;
+            }
+        }
+        return null; // Si no se encuentra el artículo
+    }
+
+
+
+    private void eliminarArticulo(){
 
     }
+
+
 
 }
 
--- Tienda/src/VistaArticulo.java
+++ Tienda/src/VistaArticulo.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 
 public class VistaArticulo {
     private Scanner scanner;
+    private ControladorArticulo controladorArticulo;
 
     public VistaArticulo(){
         scanner = new Scanner(System.in);
+        controladorArticulo = new ControladorArticulo();
     }
 
     public void mostrarMenu() {
@@ -13,7 +15,8 @@ public class VistaArticulo {
         System.out.println("1. Ver artículos");
         System.out.println("2. Agregar artículo");
         System.out.println("3. Actualizar artículo");
-        System.out.println("4. Salir");
+        System.out.println("4. Eliminar artículo");
+        System.out.println("0. Salir");
         System.out.println("-----------------");
     }
 
@@ -34,15 +37,41 @@ public class VistaArticulo {
         scanner.nextLine(); // Limpiar el buffer
         System.out.print("Ingresa el identificador del artículo: ");
         int id = scanner.nextInt();
-        System.out.println("Ingresa el nombre del artículo: ");
+        scanner.nextLine(); // Consumir el carácter de nueva línea pendiente
+        System.out.print("Ingresa el nombre del artículo: ");
         String nombre = scanner.nextLine();
+        scanner.nextLine(); // Consumir el carácter de nueva línea pendiente
         System.out.print("Ingresa el precio del artículo: ");
         double precio = scanner.nextDouble();
-        System.out.println("Ingresa el precio del artículo (proveedor): ");
+        System.out.print("Ingresa el precio del artículo (proveedor): ");
         double precioProveedor = scanner.nextDouble();
         System.out.print("Ingresa la cantidad en stock: ");
         int stock = scanner.nextInt();
         return new Articulo(id, nombre, precio, precioProveedor, stock);
+    }
+
+    public void modificarArticulo() {
+
+        // Obtener los parámetros para modificar el artículo
+        System.out.print("Ingrese el identificador del artículo a modificar: ");
+        int identificador = scanner.nextInt();
+        scanner.nextLine(); // Limpiar el buffer
+
+        System.out.print("Nuevo nombre del artículo: ");
+        String nuevoNombre = scanner.nextLine();
+
+        System.out.print("Nuevo precio al público del artículo: ");
+        double nuevoPrecioPublico = scanner.nextDouble();
+
+        System.out.print("Nuevo precio del proveedor del artículo: ");
+        double nuevoPrecioProveedor = scanner.nextDouble();
+
+        System.out.print("Nueva cantidad de existencia del artículo: ");
+        int nuevaCantidadExistencia = scanner.nextInt();
+
+        // Llamar al controlador para modificar el artículo
+        controladorArticulo.modificarArticulo(identificador, nuevoNombre, nuevoPrecioPublico, nuevoPrecioProveedor, nuevaCantidadExistencia);
+
     }
 
     public void mostrarMensaje(String mensaje) {
